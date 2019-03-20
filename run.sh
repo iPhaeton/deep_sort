d399934b8d4156edf9f56f8e0d3d6d4cf34d123a
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-03-20 19:13:31 +0300

    Define run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+python2 deep_sort_app.py \
+    --sequence_dir=./resources/sequences/intersection_test/ \
+    --detection_file=./resources/detections/sequences/intersection_test_1.npy  \
+    --min_confidence=0 \
+    --nn_budget=10 \
+    --display=True \
+    --nms_max_overlap=1 \
+    --max_cosine_distance=0.2 \
+    --max_age=10000 \
+    --max_iou_distance=0.7
