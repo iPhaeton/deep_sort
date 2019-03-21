1cfa4d5737b225fba8a892b0b7ff60a86aba4714
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-03-21 15:24:11 +0300

    Create detections from triplet_all model

--- generate_detections.sh
+++ generate_detections.sh
@@ -1,4 +1,4 @@
 python2 tools/generate_detections.py \
-    --model=../input/models/checkpoint/iteration-100.ckpt \
+    --model=../input/models/checkpoint/iteration-20.ckpt \
     --mot_dir=./resources/sequences/ \
-    --output_dir=./resources/detections/sequences/intersection_test_1.npy
+    --output_dir=./resources/detections/sequences/intersection_test_all.npy
