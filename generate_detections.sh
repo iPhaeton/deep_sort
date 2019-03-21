a59cea1818b2c97db5df8b425b70cb7159ee6801
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-03-21 11:18:45 +0300

    Define generate_detections.sh

---
+++ generate_detections.sh
@@ -0,0 +1,4 @@
+python2 tools/generate_detections.py \
+    --model=../input/models/deep_sort_cnn/mars-small128.pb \
+    --mot_dir=./resources/sequences/ \
+    --output_dir=./resources/detections/sequences/test.npy
